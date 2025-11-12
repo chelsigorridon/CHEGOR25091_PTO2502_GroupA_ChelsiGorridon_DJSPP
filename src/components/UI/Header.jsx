342f6835e84cee8aa17b5dfd47f19de3e14d343b
chelsigorridon <chelsijaimegorridon@gmail.com>
2025-11-12 18:10:57 +0200

    Added Props to header

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -1,18 +1,25 @@
 import styles from "./Header.module.css";
+import LightDarkMode from "./LightDarkMode.jsx";
 import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
+     
     <header className={styles.appHeader}>
       <h1>
-        {" "}
-        <Link to="/">
+      {" "}
+    <Link to="/">
          <img
          src="./img/Favicon.png"
           alt="Podcast App Logo"
           className={styles.logo}
           />
-         Podcast App</Link>
+         Podcast App </Link>
       </h1>
+
+     <LightDarkMode/>
+
     </header>
+
   );
 }
