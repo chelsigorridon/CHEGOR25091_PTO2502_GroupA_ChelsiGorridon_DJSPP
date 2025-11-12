6244582692b00884ec537c242893b3f6bad642d1
chelsigorridon <chelsijaimegorridon@gmail.com>
2025-11-12 14:26:32 +0200

    Added Usestate & UseEffect

---
+++ src/components/UI/LightDarkMode.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+export default function LightDarkMode() {
+
+    const [theme, setTheme]= useState (
+        localStorage.getItem("theme")||"light"
+    );
+
+    useEffect (()=> {
+        document.body.className= theme;
+        localStorage.setItem ("theme", theme);
+    }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme ((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    };
+}
