015188984a50982c45b5a32f06dc6e2543c95250
chelsigorridon <chelsijaimegorridon@gmail.com>
2025-11-17 15:03:18 +0200

    Updated Home file with Carousel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,30 +11,32 @@ import styles from "./Home.module.css";
 import { genres } from "../data";
 import { PodcastContext } from "../context/PodcastContext";
 import { useContext } from "react";
+import Carousel from "../components/UI/Carousel";
 
-/**
- * Home page of the Podcast Explorer app.
- *
- * - Displays the main podcast browsing interface.
- * - Includes search, genre filter, and sort controls.
- * - Shows a loading indicator or error message based on fetch state.
- * - Renders the podcast grid and pagination once data is loaded.
- *
- * Context:
- * - Consumes `PodcastContext` to access podcast data, loading, and error states.
- *
- * @returns {JSX.Element} The home page content with filters, results, and feedback states.
- */
 export default function Home() {
   const { podcasts, loading, error } = useContext(PodcastContext);
 
+  console.log(podcasts[0]);
+  
+const carouselItems =
+  podcasts?.map((pod) => ({
+    id: pod.id,
+    title: pod.title,
+    image: pod.image || "https://via.placeholder.com/300x400?text=No+Image",
+  })) || [];
+
   return (
     <main className={styles.main}>
+     
       <section className={styles.controls}>
         <SearchBar />
         <GenreFilter genres={genres} />
         <SortSelect />
       </section>
+
+      {!loading && !error && podcasts.length > 0 && (
+        <Carousel items={carouselItems} />
+      )}
 
       {loading && <Loading message="Loading podcasts..." />}
       {error && (
