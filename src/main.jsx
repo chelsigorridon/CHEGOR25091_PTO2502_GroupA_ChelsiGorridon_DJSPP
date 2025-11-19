fde2c0da0c21bee6c0332a2d9bc1e6f2db48f5dd
chelsigorridon <chelsijaimegorridon@gmail.com>
2025-11-19 22:51:44 +0200

    imported favourites & added Provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,18 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { PodcastProvider } from "./context/PodcastContext";
+import { FavouritesProvider } from "./context/FaveContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+     
     <PodcastProvider>
+      <FavouritesProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </FavouritesProvider>
     </PodcastProvider>
+    
   </StrictMode>
 );
