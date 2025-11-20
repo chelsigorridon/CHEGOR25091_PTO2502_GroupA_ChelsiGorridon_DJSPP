d61e54c981d6df7a8bd9804132705450bd77d98a
chelsigorridon <chelsijaimegorridon@gmail.com>
2025-11-20 09:08:27 +0200

    Fixed carousel Genres added data import

--- src/components/UI/Carousel.jsx
+++ src/components/UI/Carousel.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import styles from  "./carousel.module.css";
+import { useNavigate } from "react-router-dom";
+import { genres as genreList } from "../../data.js";
 
 export default function Carousel({ items }) {
     const [currentIndex, setCurrentIndex] = useState(0);
+     const navigate = useNavigate();
 
-     const nextSlide = () => {
+  const nextSlide = () => {
     setCurrentIndex((prev) => (prev + 1) % items.length);
   };
 
@@ -12,29 +15,51 @@ export default function Carousel({ items }) {
     setCurrentIndex((prev) => (prev - 1 + items.length) % items.length);
   };
 
+  const handleClick = (id) => {
+    navigate(`/show/${id}`);
+  };
 
-   return (
-  <div className={styles.carouselContainer}>
-    <button onClick={prevSlide} className={styles.navButton}>◀</button>
+  return (
+    <div className={styles.carouselContainer}>
+      <button onClick={prevSlide} className={styles.navButton}>
+        ◀
+      </button>
 
-    <div
-      className={styles.carouselTrack}
-      style={{ transform: `translateX(-${currentIndex * 100}%)` }}
-    >
-      {items.map((item, index) => (
-        <div
-          key={item.id || index}
-          className={`${styles.carouselItem} ${
-            index === currentIndex ? styles.active : ""
-          }`}
-        >
-          <img src={item.image} alt={item.title} />
-          <h3>{item.title}</h3>
-        </div>
-      ))}
+      <div
+        className={styles.carouselTrack}
+        style={{
+          width: `${items.length * 100}%`,
+          transform: `translateX(-${currentIndex * (100 / items.length)}%)`,
+        }}
+      >
+        {items.map((item, index) => (
+          <div
+            key={item.id || index}
+            className={styles.carouselItem}
+            onClick={() => handleClick(item.id)}
+          >
+          <img className ={styles.imageWrap} src={item.image} alt={item.title} />
+            
+            {item.genres && item.genres.length > 0 && (
+             <div className={styles.genres}>
+            {item.genres.slice(0, 3).map((genreId) => {
+            const g = genreList.find((genre) => genre.id === genreId);
+             return (
+            <span key={genreId} className={styles.genreTag}>
+          {g ? g.title : "Unknown"}
+           </span>
+           );
+           })}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+
+      <button onClick={nextSlide} className={styles.navButton}>
+        ▶
+      </button>
     </div>
-
-    <button onClick={nextSlide} className={styles.navButton}>▶</button>
-  </div>
-);
+  );
 }
+
