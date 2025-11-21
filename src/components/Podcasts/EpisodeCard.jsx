24a3622f0e792aea6c4f4d14db4afe32f67cbfce
chelsigorridon <chelsijaimegorridon@gmail.com>
2025-11-21 08:58:39 +0200

    Updated Audio Playback btn

--- src/components/Podcasts/EpisodeCard.jsx
+++ src/components/Podcasts/EpisodeCard.jsx
@@ -19,7 +19,8 @@ export default function EpisodeCard({ episode, seasonImage, index }) {
       showTitle: episode.showTitle,
       seasonNumber: episode.season,         
       episodeNumber: index + 1,             
-      seasonImage: seasonImage              
+      seasonImage: seasonImage,   
+      addedAt: new Date().toISOString(),          
     };
 
     fav ? removeFavourite(episode.episodeId) : addFavourite(favData);
@@ -40,36 +41,42 @@ export default function EpisodeCard({ episode, seasonImage, index }) {
         </p>
         <p className={styles.description}>{episode.description}</p>
       
-         <button
-        className={styles.play}
-        disabled={!audioUrl}
-        onClick={() => {
-         console.log("Playing audio:", audioUrl);
-       playAudio(audioUrl);
-     }}
-      >
-        ▶ Play Episode
-      </button>
+          
+        <button
+          className={styles.play}
+          disabled={!audioUrl}
+          onClick={() =>
+            playAudio({
+              id: episode.episodeId,
+              src: audioUrl,
+              title: episode.title,
+              showTitle: episode.showTitle,
+            })
+          }
+        >
+          ▶ Play Episode
+        </button>
       </div>
 
+     
       <button className={styles.heart} onClick={toggleFavourite}>
-         {fav ? (
-    <svg
-      className={styles.filled}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-    >
-      <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
-    </svg>
-  ) : (
-    <svg
-      className={styles.empty}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-    >
-      <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
-    </svg>
-    )}
+        {fav ? (
+          <svg
+            className={styles.filled}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+          </svg>
+        ) : (
+          <svg
+            className={styles.empty}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+          </svg>
+        )}
       </button>
     </div>
   );
