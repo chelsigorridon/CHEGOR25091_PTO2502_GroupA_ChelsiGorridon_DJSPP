d0862f11b87c9b310642d0700f424100ec0821e1
chelsigorridon <chelsijaimegorridon@gmail.com>
2025-11-21 09:06:54 +0200

    Updating AudioPlayer adding stretch goals

--- src/components/UI/AudioPlayer.jsx
+++ src/components/UI/AudioPlayer.jsx
@@ -4,7 +4,7 @@ import styles from "./AudioPlayer.module.css";
 export default function AudioPlayer() {
   const {
     isPlaying,
-    currentSrc,
+    currentEpisode,
     playAudio,
     pauseAudio,
     progress,
@@ -12,26 +12,39 @@ export default function AudioPlayer() {
     seekAudio,
   } = useAudio();
 
-  if (!currentSrc) return null;
+  
+  if (!currentEpisode) return null;
 
   return (
     <div className={styles.player}>
+      
+      
       <button
         className={styles.playBtn}
-        onClick={() => (isPlaying ? pauseAudio() : playAudio(currentSrc))}
+        onClick={() =>
+          isPlaying ? pauseAudio() : playAudio(currentEpisode)
+        }
       >
         {isPlaying ? "⏸️" : "▶️"}
       </button>
 
+      {/* Episode info */}
+      <div className={styles.info}>
+        <p className={styles.title}>{currentEpisode.title}</p>
+        <span className={styles.show}>{currentEpisode.showTitle}</span>
+      </div>
+
+      
       <input
         type="range"
         min="0"
-        max={duration}
+        max={duration || 0}
         value={progress}
         onChange={(e) => seekAudio(Number(e.target.value))}
         className={styles.slider}
       />
 
+    
       <span className={styles.time}>
         {Math.floor(progress)}s / {Math.floor(duration)}s
       </span>
